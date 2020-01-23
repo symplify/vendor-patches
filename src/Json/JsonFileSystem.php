f75b5fb2b2dab9a31d323020ff4d17b9af83999f
TomasVotruba <tomas.vot@gmail.com>
2020-01-24 00:21:01 +0100

    fix JsonFileSystem

--- src/Json/JsonFileSystem.php
+++ src/Json/JsonFileSystem.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Migrify\VendorPatches\Json;
 
+use Nette\Utils\FileSystem;
+use Nette\Utils\Json;
+
 final class JsonFileSystem
 {
     public function loadFilePathToJson(string $filePath): array
     {
-        return [];
+        $fileContent = FileSystem::read($filePath);
+
+        return Json::decode($fileContent, Json::FORCE_ARRAY);
     }
 }
