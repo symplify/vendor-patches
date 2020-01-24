f8196ff23ec8d4d85d04b6f93ae87813923cbb07
TomasVotruba <tomas.vot@gmail.com>
2020-01-24 14:35:29 +0100

    fix binary files

---
+++ src/Composer/PackageNameResolver.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\VendorPatches\Composer;
+
+use Migrify\VendorPatches\Exception\ShouldNotHappenException;
+use Migrify\VendorPatches\FileSystem\PathResolver;
+use Migrify\VendorPatches\Json\JsonFileSystem;
+use Symplify\SmartFileSystem\FileSystemGuard;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class PackageNameResolver
+{
+    /**
+     * @var JsonFileSystem
+     */
+    private $jsonFileSystem;
+
+    /**
+     * @var PathResolver
+     */
+    private $pathResolver;
+
+    /**
+     * @var FileSystemGuard
+     */
+    private $fileSystemGuard;
+
+    public function __construct(
+        JsonFileSystem $jsonFileSystem,
+        PathResolver $pathResolver,
+        FileSystemGuard $fileSystemGuard
+    ) {
+        $this->jsonFileSystem = $jsonFileSystem;
+        $this->pathResolver = $pathResolver;
+        $this->fileSystemGuard = $fileSystemGuard;
+    }
+
+    public function resolveFromFileInfo(SmartFileInfo $vendorFile): string
+    {
+        $packageComposerJsonFilePath = $this->getPackageComposerJsonFilePath($vendorFile);
+
+        $composerJson = $this->jsonFileSystem->loadFilePathToJson($packageComposerJsonFilePath);
+        if (! isset($composerJson['name'])) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $composerJson['name'];
+    }
+
+    private function getPackageComposerJsonFilePath(SmartFileInfo $vendorFile): string
+    {
+        $vendorPackageDirectory = $this->pathResolver->resolveVendor($vendorFile->getRealPath());
+        $packageComposerJsonFilePath = $vendorPackageDirectory . '/composer.json';
+        $this->fileSystemGuard->ensureFileExists($packageComposerJsonFilePath, __METHOD__);
+
+        return $packageComposerJsonFilePath;
+    }
+}
