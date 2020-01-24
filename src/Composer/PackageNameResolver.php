55e2d49e871cc2ec57d09df179606d2c0f854478
TomasVotruba <tomas.vot@gmail.com>
2020-01-24 09:31:50 +0100

    improve JsonFileSystem coverage

---
+++ src/Composer/PackageNameResolver.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\VendorPatches\Composer;
+
+use Migrify\VendorPatches\Exception\ShouldNotHappenException;
+use Migrify\VendorPatches\FileSystem\PathResolver;
+use Migrify\VendorPatches\Json\JsonFileSystem;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class PackageNameResolver
+{
+    /**
+     * @var JsonFileSystem
+     */
+    private $jsonFileSystem;
+
+    /**
+     * @var PathResolver
+     */
+    private $pathResolver;
+
+    public function __construct(JsonFileSystem $jsonFileSystem, PathResolver $pathResolver)
+    {
+        $this->jsonFileSystem = $jsonFileSystem;
+        $this->pathResolver = $pathResolver;
+    }
+
+    public function resolveFromFileInfo(SmartFileInfo $vendorFile): string
+    {
+        $vendorPackageDirectory = $this->resolveVendorPackageDirectory($vendorFile);
+
+        $packageComposerJsonFilePath = $vendorPackageDirectory . '/composer.json';
+        if (! file_exists($packageComposerJsonFilePath)) {
+            throw new ShouldNotHappenException();
+        }
+
+        $composerJson = $this->jsonFileSystem->loadFilePathToJson($packageComposerJsonFilePath);
+        if (! isset($composerJson['name'])) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $composerJson['name'];
+    }
+
+    private function resolveVendorPackageDirectory(SmartFileInfo $vendorFile): string
+    {
+        return $this->pathResolver->resolveVendor($vendorFile->getRealPath());
+    }
+}
---
+++ tests/Json/JsonFileSystemTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\VendorPatches\Tests\Json;
+
+use Migrify\VendorPatches\HttpKernel\VendorPatchesKernel;
+use Migrify\VendorPatches\Json\JsonFileSystem;
+use Nette\Utils\FileSystem;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+
+final class JsonFileSystemTest extends AbstractKernelTestCase
+{
+    /**
+     * @var JsonFileSystem
+     */
+    private $jsonFileSystem;
+
+    protected function setUp(): void
+    {
+        self::bootKernel(VendorPatchesKernel::class);
+        $this->jsonFileSystem = self::$container->get(JsonFileSystem::class);
+    }
+
+    public function testLoadFilePathToJson(): void
+    {
+        $json = $this->jsonFileSystem->loadFilePathToJson(__DIR__ . '/JsonFileSystemSource/some_file.json');
+        $this->assertSame(['key' => 'value'], $json);
+    }
+
+    public function testWriteJsonToFilePath(): void
+    {
+        $filePath = __DIR__ . '/JsonFileSystemSource/temp_file.json';
+
+        $this->jsonFileSystem->writeJsonToFilePath(['hey' => 'you'], $filePath);
+        $this->assertFileExists($filePath);
+
+        $expectedFilePath = __DIR__ . '/JsonFileSystemSource/expected_temp_file.json';
+        $this->assertFileEquals($expectedFilePath, $filePath);
+
+        FileSystem::delete($filePath);
+    }
+}
