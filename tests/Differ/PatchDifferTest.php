929b18653e10470edf4e45ba248d4d414ebc4033
TomasVotruba <tomas.vot@gmail.com>
2020-04-09 12:27:07 +0200

    fix token name

---
+++ tests/Differ/PatchDifferTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\VendorPatches\Tests\Differ;
+
+use Migrify\VendorPatches\Differ\PatchDiffer;
+use Migrify\VendorPatches\HttpKernel\VendorPatchesKernel;
+use Symplify\PackageBuilder\Tests\AbstractKernelTestCase;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+final class PatchDifferTest extends AbstractKernelTestCase
+{
+    /**
+     * @var PatchDiffer
+     */
+    private $patchDiffer;
+
+    protected function setUp(): void
+    {
+        self::bootKernel(VendorPatchesKernel::class);
+
+        $this->patchDiffer = self::$container->get(PatchDiffer::class);
+    }
+
+    public function test(): void
+    {
+        $beforeFile = new SmartFileInfo(__DIR__ . '/PatchDifferSource/vendor/some/package/before_file.php');
+        $afterFile = new SmartFileInfo(__DIR__ . '/PatchDifferSource/vendor-changed/some/package/after_file.php');
+
+        $diff = $this->patchDiffer->diff($beforeFile, $afterFile);
+
+        $this->assertStringEqualsFile(__DIR__ . '/PatchDifferFixture/expected_diff.php', $diff);
+    }
+}
