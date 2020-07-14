1a45fcfac06b5bd0e33f09a5732816351c4741d7
TomasVotruba <tomas.vot@gmail.com>
2020-07-15 01:20:22 +0200

    make use of SmartFileSystem

--- src/Json/JsonFileSystem.php
+++ src/Json/JsonFileSystem.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace Migrify\VendorPatches\Json;
 
 use Nette\Utils\Arrays;
-use Nette\Utils\FileSystem;
 use Nette\Utils\Json;
 use Symplify\SmartFileSystem\FileSystemGuard;
+use Symplify\SmartFileSystem\SmartFileSystem;
 
 final class JsonFileSystem
 {
@@ -16,23 +16,29 @@ final class JsonFileSystem
      */
     private $fileSystemGuard;
 
-    public function __construct(FileSystemGuard $fileSystemGuard)
+    /**
+     * @var SmartFileSystem
+     */
+    private $smartFileSystem;
+
+    public function __construct(FileSystemGuard $fileSystemGuard, SmartFileSystem $smartFileSystem)
     {
         $this->fileSystemGuard = $fileSystemGuard;
+        $this->smartFileSystem = $smartFileSystem;
     }
 
     public function loadFilePathToJson(string $filePath): array
     {
         $this->fileSystemGuard->ensureFileExists($filePath, __METHOD__);
 
-        $fileContent = FileSystem::read($filePath);
+        $fileContent = $this->smartFileSystem->readFile($filePath);
         return Json::decode($fileContent, Json::FORCE_ARRAY);
     }
 
     public function writeJsonToFilePath(array $jsonArray, string $filePath): void
     {
         $jsonContent = Json::encode($jsonArray, Json::PRETTY) . PHP_EOL;
-        FileSystem::write($filePath, $jsonContent);
+        $this->smartFileSystem->dumpFile($filePath, $jsonContent);
     }
 
     public function mergeArrayToJsonFile(string $filePath, array $newJsonArray): void
