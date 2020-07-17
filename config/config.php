e952b4640bb52bf9bfe54443dbb1e18000595e80
James Walker <jh_walker@outlook.com>
2020-07-17 17:57:34 +0100

    vendor-patches add additional autoload include path
    Refer to #85. When installed with Composer, `vendor/bin/vendor-patches` is symlink from `vendor/migrify/vendor-patches/bin/vendor-patches`, so the correct path to the project's `autoload.php` is three levels up.

---
+++ config/config.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use SebastianBergmann\Diff\Differ;
+use SebastianBergmann\Diff\Output\UnifiedDiffOutputBuilder;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symplify\PackageBuilder\Console\Style\SymfonyStyleFactory;
+use Symplify\SmartFileSystem\FileSystemGuard;
+use Symplify\SmartFileSystem\Finder\FinderSanitizer;
+use Symplify\SmartFileSystem\SmartFileSystem;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $services = $containerConfigurator->services();
+
+    $services->defaults()
+        ->public()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->load('Migrify\VendorPatches\\', __DIR__ . '/../src')
+        ->exclude([__DIR__ . '/../src/HttpKernel/*', __DIR__ . '/../src/ValueObject/*']);
+
+    $services->set(UnifiedDiffOutputBuilder::class)
+        ->args(['$addLineNumbers' => true]);
+
+    $services->set(Differ::class)
+        ->args(['$outputBuilder' => ref(UnifiedDiffOutputBuilder::class)]);
+
+    $services->set(FinderSanitizer::class);
+
+    $services->set(FileSystemGuard::class);
+
+    $services->set(SymfonyStyleFactory::class);
+
+    $services->set(SymfonyStyle::class)
+        ->factory([ref(SymfonyStyleFactory::class), 'create']);
+
+    $services->set(SmartFileSystem::class);
+};
