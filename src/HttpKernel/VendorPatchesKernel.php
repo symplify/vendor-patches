b98af78543b6082137e8d3d715716721b2c7a43b
Tomas Votruba <tomas.vot@gmail.com>
2020-09-08 23:54:19 +0200

    [CIToGithubActions] Init (#156)

--- src/HttpKernel/VendorPatchesKernel.php
+++ src/HttpKernel/VendorPatchesKernel.php
@@ -4,33 +4,13 @@ declare(strict_types=1);
 
 namespace Migrify\VendorPatches\HttpKernel;
 
-use Migrify\MigrifyKernel\Bundle\MigrifyKernelBundle;
+use Migrify\MigrifyKernel\HttpKernel\AbstractMigrifyKernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\BundleInterface;
-use Symfony\Component\HttpKernel\Kernel;
 use Symplify\AutoBindParameter\DependencyInjection\CompilerPass\AutoBindParameterCompilerPass;
 
-final class VendorPatchesKernel extends Kernel
+final class VendorPatchesKernel extends AbstractMigrifyKernel
 {
-    /**
-     * @return BundleInterface[]
-     */
-    public function registerBundles(): iterable
-    {
-        return [new MigrifyKernelBundle()];
-    }
-
-    public function getCacheDir(): string
-    {
-        return sys_get_temp_dir() . '/vendor_patches_generator';
-    }
-
-    public function getLogDir(): string
-    {
-        return sys_get_temp_dir() . '/vendor_patches_generator_log';
-    }
-
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load(__DIR__ . '/../../config/config.php');
