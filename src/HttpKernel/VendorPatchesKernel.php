8ac0f0c46ed1ad45180e7ab41adddf13f7cde3d0
Tomas Votruba <tomas.vot@gmail.com>
2020-09-17 18:17:07 +0200

    drop AutoBindParmaeter from vendor patches (#174)

--- src/HttpKernel/VendorPatchesKernel.php
+++ src/HttpKernel/VendorPatchesKernel.php
@@ -6,18 +6,11 @@ namespace Migrify\VendorPatches\HttpKernel;
 
 use Migrify\MigrifyKernel\HttpKernel\AbstractMigrifyKernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symplify\AutoBindParameter\DependencyInjection\CompilerPass\AutoBindParameterCompilerPass;
 
 final class VendorPatchesKernel extends AbstractMigrifyKernel
 {
     public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load(__DIR__ . '/../../config/config.php');
-    }
-
-    protected function build(ContainerBuilder $containerBuilder): void
-    {
-        $containerBuilder->addCompilerPass(new AutoBindParameterCompilerPass());
     }
 }
