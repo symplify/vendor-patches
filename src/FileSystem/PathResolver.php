d09e5f01bfdb7e3e8f9a311d9d0bb8d33cc1d7c2
Tomas Votruba <tomas.vot@gmail.com>
2020-09-17 14:21:34 +0200

    [ConfigPretifier] Decouple new package (#169)

--- src/FileSystem/PathResolver.php
+++ src/FileSystem/PathResolver.php
@@ -14,11 +14,11 @@ final class PathResolver
      * @see https://regex101.com/r/KhzCSu/1
      * @var string
      */
-    private const VENDOR_PACKAGE_DIRECTORY_PATTERN = '#^(?<vendor_package_directory>.*?vendor\/(\w|\.|\-)+\/(\w|\.|\-)+)\/#si';
+    private const VENDOR_PACKAGE_DIRECTORY_REGEX = '#^(?<vendor_package_directory>.*?vendor\/(\w|\.|\-)+\/(\w|\.|\-)+)\/#si';
 
     public function resolveVendorDirectory(SmartFileInfo $fileInfo): string
     {
-        $match = Strings::match($fileInfo->getRealPath(), self::VENDOR_PACKAGE_DIRECTORY_PATTERN);
+        $match = Strings::match($fileInfo->getRealPath(), self::VENDOR_PACKAGE_DIRECTORY_REGEX);
         if (! isset($match['vendor_package_directory'])) {
             throw new ShouldNotHappenException('Could not resolve vendor package directory');
         }
