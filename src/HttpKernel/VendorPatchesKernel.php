13da392080bc4fbe645f5811cd38ebfd078f64d2
TomasVotruba <tomas.vot@gmail.com>
2020-09-17 19:40:20 +0200

    update sonar paths

---
+++ src/HttpKernel/VendorPatchesKernel.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Migrify\VendorPatches\HttpKernel;
+
+use Migrify\MigrifyKernel\HttpKernel\AbstractMigrifyKernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+final class VendorPatchesKernel extends AbstractMigrifyKernel
+{
+    public function registerContainerConfiguration(LoaderInterface $loader): void
+    {
+        $loader->load(__DIR__ . '/../../config/config.php');
+    }
+}
