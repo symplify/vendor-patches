c65a1bae16c662e030904a6d6eea1cbb0c12cfd4
GitHub Action <action@github.com>
2022-06-10 09:48:26 +0000

    Updated Vendor Patches to commit c93fd9e480da7055269e7dd430cf663289004234
    https://github.com/symplify/symplify/commit/c93fd9e480da7055269e7dd430cf663289004234 use united build-scoped.sh name, to ease mimics

--- src/Differ/PatchDiffer.php
+++ src/Differ/PatchDiffer.php
@@ -1,15 +1,13 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
+namespace VendorPatches20220610\Symplify\VendorPatches\Differ;
 
-namespace Symplify\VendorPatches\Differ;
-
-use Nette\Utils\Strings;
-use SebastianBergmann\Diff\Differ;
-use Symplify\SmartFileSystem\SmartFileInfo;
-use Symplify\SymplifyKernel\Exception\ShouldNotHappenException;
-use Symplify\VendorPatches\ValueObject\OldAndNewFileInfo;
-
+use VendorPatches20220610\Nette\Utils\Strings;
+use VendorPatches20220610\SebastianBergmann\Diff\Differ;
+use VendorPatches20220610\Symplify\SmartFileSystem\SmartFileInfo;
+use VendorPatches20220610\Symplify\SymplifyKernel\Exception\ShouldNotHappenException;
+use VendorPatches20220610\Symplify\VendorPatches\ValueObject\OldAndNewFileInfo;
 /**
  * @see \Symplify\VendorPatches\Tests\Differ\PatchDifferTest
  */
@@ -19,45 +17,40 @@ final class PatchDiffer
      * @see https://regex101.com/r/0O5NO1/4
      * @var string
      */
-    private const LOCAL_PATH_REGEX = '#vendor\/[^\/]+\/[^\/]+\/(?<local_path>.*?)$#is';
-
+    private const LOCAL_PATH_REGEX = '#vendor\\/[^\\/]+\\/[^\\/]+\\/(?<local_path>.*?)$#is';
     /**
      * @see https://regex101.com/r/vNa7PO/1
      * @var string
      */
     private const START_ORIGINAL_REGEX = '#^--- Original#';
-
     /**
      * @see https://regex101.com/r/o8C90E/1
      * @var string
      */
-    private const START_NEW_REGEX = '#^\+\+\+ New#m';
-
-    public function __construct(
-        private Differ $differ
-    ) {
+    private const START_NEW_REGEX = '#^\\+\\+\\+ New#m';
+    /**
+     * @var \SebastianBergmann\Diff\Differ
+     */
+    private $differ;
+    public function __construct(Differ $differ)
+    {
+        $this->differ = $differ;
     }
-
-    public function diff(OldAndNewFileInfo $oldAndNewFileInfo): string
+    public function diff(OldAndNewFileInfo $oldAndNewFileInfo) : string
     {
         $oldFileInfo = $oldAndNewFileInfo->getOldFileInfo();
         $newFileInfo = $oldAndNewFileInfo->getNewFileInfo();
-
         $diff = $this->differ->diff($oldFileInfo->getContents(), $newFileInfo->getContents());
-
         $patchedFileRelativePath = $this->resolveFileInfoPathRelativeFilePath($newFileInfo);
-
         $clearedDiff = Strings::replace($diff, self::START_ORIGINAL_REGEX, '--- /dev/null');
         return Strings::replace($clearedDiff, self::START_NEW_REGEX, '+++ ' . $patchedFileRelativePath);
     }
-
-    private function resolveFileInfoPathRelativeFilePath(SmartFileInfo $beforeFileInfo): string
+    private function resolveFileInfoPathRelativeFilePath(SmartFileInfo $beforeFileInfo) : string
     {
         $match = Strings::match($beforeFileInfo->getRealPath(), self::LOCAL_PATH_REGEX);
-        if (! isset($match['local_path'])) {
+        if (!isset($match['local_path'])) {
             throw new ShouldNotHappenException();
         }
-
         return '../' . $match['local_path'];
     }
 }
--- src/Kernel/VendorPatchesKernel.php
+++ src/Kernel/VendorPatchesKernel.php
@@ -1,23 +1,20 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types=1);
+namespace VendorPatches20220610\Symplify\VendorPatches\Kernel;
 
-namespace Symplify\VendorPatches\Kernel;
-
-use Psr\Container\ContainerInterface;
-use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonManipulatorConfig;
-use Symplify\SymplifyKernel\HttpKernel\AbstractSymplifyKernel;
-
+use VendorPatches20220610\Psr\Container\ContainerInterface;
+use VendorPatches20220610\Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonManipulatorConfig;
+use VendorPatches20220610\Symplify\SymplifyKernel\HttpKernel\AbstractSymplifyKernel;
 final class VendorPatchesKernel extends AbstractSymplifyKernel
 {
     /**
      * @param string[] $configFiles
      */
-    public function createFromConfigs(array $configFiles): ContainerInterface
+    public function createFromConfigs(array $configFiles) : ContainerInterface
     {
         $configFiles[] = __DIR__ . '/../../config/config.php';
         $configFiles[] = ComposerJsonManipulatorConfig::FILE_PATH;
-
         return $this->create($configFiles);
     }
 }
