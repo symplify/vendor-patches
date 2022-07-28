643e36b3f146aff14b27c2414a9ad0fb68ab263c
GitHub Action <action@github.com>
2022-07-28 20:23:37 +0000

    Updated Vendor Patches to commit d1e786123d40523bc15e1e703e79ea1f457decf9
    https://github.com/symplify/symplify/commit/d1e786123d40523bc15e1e703e79ea1f457decf9 Add nelmo_cors bundle configuration (#4282)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -9,4 +9,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit92e22842cfd4cfe14bb8fdfc85d80cc4::getLoader();
+return ComposerAutoloaderInit920b9c481ec42ce15f31e552bc4c86a5::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit92e22842cfd4cfe14bb8fdfc85d80cc4
+class ComposerStaticInit920b9c481ec42ce15f31e552bc4c86a5
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
@@ -717,9 +717,9 @@ class ComposerStaticInit92e22842cfd4cfe14bb8fdfc85d80cc4
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit92e22842cfd4cfe14bb8fdfc85d80cc4::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit92e22842cfd4cfe14bb8fdfc85d80cc4::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit92e22842cfd4cfe14bb8fdfc85d80cc4::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit920b9c481ec42ce15f31e552bc4c86a5::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit920b9c481ec42ce15f31e552bc4c86a5::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit920b9c481ec42ce15f31e552bc4c86a5::$classMap;
 
         }, null, ClassLoader::class);
     }
