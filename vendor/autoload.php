b663406b89dd62fbf9d78f88911c7edde391d439
GitHub Action <action@github.com>
2022-08-21 02:10:35 +0000

    Updated Vendor Patches to commit af1f7145c4164c4c851de9b6816f68fc5ddfbf18
    https://github.com/symplify/symplify/commit/af1f7145c4164c4c851de9b6816f68fc5ddfbf18 [automated] Re-Generate Docs (#4359)

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -9,4 +9,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite8115cd3ace5c001a609403e2400c136::getLoader();
+return ComposerAutoloaderInitb7aed6e0ede2359ce47f9cb0b578a996::getLoader();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInite8115cd3ace5c001a609403e2400c136
+class ComposerStaticInitb7aed6e0ede2359ce47f9cb0b578a996
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
@@ -717,9 +717,9 @@ class ComposerStaticInite8115cd3ace5c001a609403e2400c136
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInite8115cd3ace5c001a609403e2400c136::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInite8115cd3ace5c001a609403e2400c136::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInite8115cd3ace5c001a609403e2400c136::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitb7aed6e0ede2359ce47f9cb0b578a996::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitb7aed6e0ede2359ce47f9cb0b578a996::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitb7aed6e0ede2359ce47f9cb0b578a996::$classMap;
 
         }, null, ClassLoader::class);
     }
