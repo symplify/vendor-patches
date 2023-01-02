05756777d4a45c37c0ac996e760a70f70f2c84e3
Tomas Votruba <tomas.vot@gmail.com>
2023-01-03 00:33:38 +0100

    misc

--- ecs.php
+++ ecs.php
@@ -9,6 +9,8 @@ return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->paths([
         __DIR__ . '/ecs.php',
         __DIR__ . '/rector.php',
+        __DIR__ . '/monorepo-builder.php',
+        __DIR__ . '/easy-ci.php',
         __DIR__ . '/config',
         __DIR__ . '/src',
         __DIR__ . '/tests',
