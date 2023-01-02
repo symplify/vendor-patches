ef73ea7b9842d921189162dceca1b257edc0b9d2
Tomas Votruba <tomas.vot@gmail.com>
2023-01-03 00:31:39 +0100

    update configs

--- ecs.php
+++ ecs.php
@@ -6,7 +6,13 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
+    $ecsConfig->paths([
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/rector.php',
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
 
     $ecsConfig->sets([
         SetList::COMMON,
