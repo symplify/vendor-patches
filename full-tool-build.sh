92dbea95364323b0ec0c4855f441db08b2087af4
Tomas Votruba <tomas.vot@gmail.com>
2024-01-01 21:33:24 +0000

    fix

--- full-tool-build.sh
+++ full-tool-build.sh
@@ -12,7 +12,7 @@ rm -rf tests
 # downgrade with rector
 mkdir rector-local
 composer require rector/rector --working-dir rector-local
-rector-local/vendor/bin/rector process bin src packages vendor --config build/rector-downgrade-php-72.php --ansi
+rector-local/vendor/bin/rector process bin src vendor --config build/rector-downgrade-php-72.php --ansi
 
 # prefix
 sh prefix-code.sh
