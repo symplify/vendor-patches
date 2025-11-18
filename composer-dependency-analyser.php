4430ab1c783af2230bf12d67285e78589ee09016
Tomas Votruba <tomas.vot@gmail.com>
2025-11-18 00:20:43 +0000

    bump deps (#42)
    * bump deps
    
    * add composer unused deps

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->ignoreErrorsOnPackage('cweagans/composer-patches', [ErrorType::UNUSED_DEPENDENCY]);
