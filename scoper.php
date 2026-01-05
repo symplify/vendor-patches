75547747d2da792de61191f6f9152a6f6be6a26a
Tomas Votruba <tomas.vot@gmail.com>
2026-01-05 15:29:00 +0100

    simpler patcher

--- scoper.php
+++ scoper.php
@@ -2,72 +2,8 @@
 
 declare(strict_types=1);
 
-use Entropy\Utils\Regex;
-use Isolated\Symfony\Component\Finder\Finder;
-
-require __DIR__ . '/vendor/autoload.php';
-
-$nowDateTime = new DateTime('now');
-$timestamp = $nowDateTime->format('Ym');
-
 // @see https://github.com/humbug/php-scoper/blob/master/docs/further-reading.md
-
-$polyfillsBootstraps = array_map(
-    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
-    iterator_to_array(
-        Finder::create()
-            ->files()
-            ->in(__DIR__ . '/vendor/symfony/polyfill-*')
-            ->name('bootstrap*.php'),
-        false,
-    ),
-);
-
-$polyfillsStubs = array_map(
-    static fn (SplFileInfo $fileInfo) => $fileInfo->getPathname(),
-    iterator_to_array(
-        Finder::create()
-            ->files()
-            ->in(__DIR__ . '/vendor/symfony/polyfill-*/Resources/stubs')
-            ->name('*.php'),
-        false,
-    ),
-);
-
-// see https://github.com/humbug/php-scoper
 return [
-    'prefix' => 'VendorPatches' . $timestamp,
-    'expose-constants' => ['#^SYMFONY\_[\p{L}_]+$#'],
+    'prefix' => 'VendorPatches' . date('Ym'),
     'exclude-namespaces' => ['#^Symplify\\\\VendorPatches#'],
-    'exclude-files' => [
-        // do not prefix "trigger_deprecation" from symfony - https://github.com/symfony/symfony/commit/0032b2a2893d3be592d4312b7b098fb9d71aca03
-        // these paths are relative to this file location, so it should be in the root directory
-        'vendor/symfony/deprecation-contracts/function.php',
-        ...$polyfillsBootstraps,
-        ...$polyfillsStubs,
-    ],
-    'patchers' => [
-        // scope symfony configs
-        function (string $filePath, string $prefix, string $content): string {
-            if (! Regex::match($filePath, '#(packages|config|services)\.php$#')) {
-                return $content;
-            }
-
-            // fix symfony config load scoping, except EasyCI
-            $content = Regex::replace(
-                $content,
-                '#load\(\'Symplify\\\\\\\\(?<package_name>[A-Za-z]+)#',
-                function (array $match) use ($prefix) {
-                    if (in_array($match['package_name'], ['VendorPatches'], true)) {
-                        // skip
-                        return $match[0];
-                    }
-
-                    return 'load(\'' . $prefix . '\Symplify\\' . $match['package_name'];
-                }
-            );
-
-            return $content;
-        },
-    ],
 ];
