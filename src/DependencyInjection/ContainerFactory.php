728e33d000ff454c4b37febb5edd1ede7b928296
Tomas Votruba <tomas.vot@gmail.com>
2026-01-05 14:42:35 +0100

    Make use of entropy container (#49)
    * less deps
    
    * use container

--- src/DependencyInjection/ContainerFactory.php
+++ src/DependencyInjection/ContainerFactory.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Symplify\VendorPatches\DependencyInjection;
 
-use Illuminate\Container\Container;
+use Entropy\Container\Container;
 use SebastianBergmann\Diff\Differ;
 use SebastianBergmann\Diff\Output\UnifiedDiffOutputBuilder;
 use Symfony\Component\Console\Application;
@@ -15,22 +15,19 @@ use Symplify\VendorPatches\Command\GenerateCommand;
 
 final class ContainerFactory
 {
-    /**
-     * @api used in bin/vendor-patches.php
-     */
     public function create(): Container
     {
         $container = new Container();
 
         // console
-        $container->singleton(SymfonyStyle::class, static function (): SymfonyStyle {
+        $container->service(SymfonyStyle::class, static function (): SymfonyStyle {
             $arrayInput = new ArrayInput([]);
             $consoleOutput = new ConsoleOutput();
             return new SymfonyStyle($arrayInput, $consoleOutput);
         });
 
         // application
-        $container->singleton(Application::class, static function (Container $container): Application {
+        $container->service(Application::class, static function (Container $container): Application {
             $application = new Application();
 
             $generateCommand = $container->make(GenerateCommand::class);
@@ -48,12 +45,12 @@ final class ContainerFactory
         });
 
         // differ
-        $container->singleton(
+        $container->service(
             UnifiedDiffOutputBuilder::class,
             static fn (): UnifiedDiffOutputBuilder => new UnifiedDiffOutputBuilder("--- Original\n+++ New\n", true)
         );
 
-        $container->singleton(
+        $container->service(
             Differ::class,
             static fn (Container $container): Differ => new Differ($container->make(UnifiedDiffOutputBuilder::class))
         );
