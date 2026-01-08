ef477b395ccedd1d86e5e1236281165ab6baca2f
Marcus Müller <25648755+M-arcus@users.noreply.github.com>
2026-01-08 18:56:38 +0100

    Force unescaped slashes when adjusting composer.json (#52)
    * Force unescaped slashes when adjusting composer.json
    
    * Add test for unescaped slash behavior

--- tests/Composer/ComposerPatchesConfigurationUpdater/ComposerPatchesConfigurationUpdaterTest.php
+++ tests/Composer/ComposerPatchesConfigurationUpdater/ComposerPatchesConfigurationUpdaterTest.php
@@ -46,4 +46,35 @@ final class ComposerPatchesConfigurationUpdaterTest extends AbstractTestCase
             ],
         ], $patchesFileJson);
     }
+
+    public function testComposerJsonAndPrint(): void
+    {
+        $composerPatchesConfigurationUpdater = $this->make(ComposerPatchesConfigurationUpdater::class);
+
+        // no changes expected
+        $composerExtraPatches = [];
+        $composerJsonFilePath = __DIR__ . '/Fixture/composer.already_has_patches.json';
+
+        // prepare temp file
+        $tempFilePath = tempnam(sys_get_temp_dir(), 'symplify_vendor_patches_composer_json_');
+        if ($tempFilePath === false) {
+            $this->markTestSkipped('Could not create temporary file.');
+        }
+
+        copy($composerJsonFilePath, $tempFilePath);
+        if (! copy($composerJsonFilePath, $tempFilePath)) {
+            unlink($tempFilePath);
+            $this->markTestSkipped('Failed to copy fixture to temporary file');
+        }
+
+        try {
+            $composerPatchesConfigurationUpdater->updateComposerJsonAndPrint($tempFilePath, $composerExtraPatches);
+
+            $this->assertFileEquals($composerJsonFilePath, $tempFilePath);
+        } finally {
+            if (file_exists($tempFilePath)) {
+                unlink($tempFilePath);
+            }
+        }
+    }
 }
