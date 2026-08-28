bdccc16509d5be1c6e9a2f20c3fe0b6d86195931
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2026-08-29 00:26:51 +0200

    Fix broken composer install and clean up stale/deprecated dev tooling

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,7 +3,5 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
-use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
-return (new Configuration())
-    ->ignoreErrorsOnPackage('cweagans/composer-patches', [ErrorType::UNUSED_DEPENDENCY]);
+return new Configuration();
--- ecs.php
+++ ecs.php
@@ -7,4 +7,4 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 return ECSConfig::configure()
     ->withPaths([__DIR__ . '/bin', __DIR__ . '/src', __DIR__ . '/tests'])
     ->withRootFiles()
-    ->withPreparedSets(psr12: true, common: true, symplify: true);
+    ->withPreparedSets(psr12: true, common: true);
