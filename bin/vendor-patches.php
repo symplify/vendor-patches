d87cbf9938dc9fd4f7037a29ec4716ddc4d1a94c
Eric Durand-Tremblay <eric.durand-tremblay@equisoft.com>
2026-09-16 10:49:48 -0400

    Update dev dependencies

--- bin/vendor-patches.php
+++ bin/vendor-patches.php
@@ -29,5 +29,6 @@ $container = ContainerFactory::create();
 
 $application = $container->make(ConsoleApplication::class);
 
+/** @phpstan-ignore variable.undefined */
 $exitCode = $application->run($argv);
 exit($exitCode);
